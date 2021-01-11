c074f9091edd6c98d9509067596655ff3c67c346
Reeshul <reeshul@hotmail.com>
2021-01-11 15:41:44 +0000

    added balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,11 @@
 require 'oystercard'
 
 describe Oystercard do
+
+  subject(:oystercard) { Oystercard.new }
+
+  it 'allows money to be topped up' do
+    expect(oystercard.balance).to eq 0 
+  end
     
 end
