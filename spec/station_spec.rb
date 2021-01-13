fbef2fc89063ffd0115289b25411d112b43a86b4
Reeshul <reeshul@hotmail.com>
2021-01-13 16:06:39 +0000

    created a station class to show each station's name and zone

---
+++ spec/station_spec.rb
@@ -0,0 +1,14 @@
+require 'station'
+
+describe 'Station' do
+
+  subject(:station) { Station.new(name: "Stanmore", zone: 5) }
+
+  it 'has a name' do
+    expect(station.name).to eq "Stanmore"
+  end
+
+  it 'has a zone' do
+    expect(station.zone).to eq 5
+  end
+end
